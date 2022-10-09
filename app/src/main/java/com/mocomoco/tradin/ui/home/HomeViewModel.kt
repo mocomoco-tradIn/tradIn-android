de9bad22245f8a6a22fe7685b1c03002be02203e
sorea.no <sorae.no@thingsflow.com>
2022-10-10 01:53:05 +0900

    Feat: Add WipScreen

--- app/src/main/java/com/mocomoco/tradin/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/mocomoco/tradin/ui/home/HomeViewModel.kt
@@ -1,6 +1,6 @@
 package com.mocomoco.tradin.ui.home
 
-import base.BaseViewModel
+import com.mocomoco.tradin.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 

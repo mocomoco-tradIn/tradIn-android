0b542aeba46760c1d079de8a9cff9219c4321e18
sorea.no <sorae.no@thingsflow.com>
2022-10-22 16:08:30 +0900

    Feat: 바텀 탭 아이콘 업데이트

---
+++ app/src/main/java/com/mocomoco/tradin/ui/common/TradInToolbar.kt
@@ -0,0 +1,20 @@
+package com.mocomoco.tradin.ui.common
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+class TradInToolbar {
+
+}
+
+@Composable
+fun TradInToolbar(
+    modifier: Modifier = Modifier,
+    title: String,
+    rightButtons: List<@Composable () -> Unit>,
+    showTitle: Boolean,
+    showBack: Boolean,
+) {
+
+
+}

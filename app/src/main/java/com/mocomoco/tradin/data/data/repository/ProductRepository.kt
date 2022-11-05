4e7266b0d1ca6602b82cc9327be1bc1d250c9bd6
sorea.no <sorae.no@thingsflow.com>
2022-11-06 01:43:48 +0900

    Feat: Add Product Repository

--- app/src/main/java/com/mocomoco/tradin/data/data/repository/ProductRepository.kt
+++ app/src/main/java/com/mocomoco/tradin/data/data/repository/ProductRepository.kt
@@ -1,11 +1,19 @@
 package com.mocomoco.tradin.data.data.repository
 
 import com.mocomoco.tradin.data.data.dto.request_body.AddProductBody
+import com.mocomoco.tradin.data.data.dto.request_body.PutProductBody
 import com.mocomoco.tradin.data.data.dto.response.ImageUrlDto
+import com.mocomoco.tradin.data.data.dto.response.ProductDto
+import com.mocomoco.tradin.data.data.dto.response.history.ProductHistoryDto
+import com.mocomoco.tradin.data.data.dto.response.products.ProductsDto
 import okhttp3.MultipartBody
-import retrofit2.Response
 
 interface ProductRepository {
     suspend fun postUploadProduct(body: AddProductBody)
     suspend fun postUploadImage(body: MultipartBody.Part): ImageUrlDto
+    suspend fun getProducts(lastId: Int): ProductsDto
+    suspend fun getProductDetails(id: Int): ProductDto
+    suspend fun putProduct(body: PutProductBody)
+    suspend fun deleteProduct(id: Int)
+    suspend fun getHistory(): ProductHistoryDto
 }
--- app/src/main/java/com/mocomoco/tradin/data/data/repository/impl/ProductRepositoryImpl.kt
+++ app/src/main/java/com/mocomoco/tradin/data/data/repository/impl/ProductRepositoryImpl.kt
@@ -2,7 +2,11 @@ package com.mocomoco.tradin.data.data.repository.impl
 
 import com.mocomoco.tradin.data.common.handleResponse
 import com.mocomoco.tradin.data.data.dto.request_body.AddProductBody
+import com.mocomoco.tradin.data.data.dto.request_body.PutProductBody
 import com.mocomoco.tradin.data.data.dto.response.ImageUrlDto
+import com.mocomoco.tradin.data.data.dto.response.ProductDto
+import com.mocomoco.tradin.data.data.dto.response.history.ProductHistoryDto
+import com.mocomoco.tradin.data.data.dto.response.products.ProductsDto
 import com.mocomoco.tradin.data.data.repository.ProductRepository
 import com.mocomoco.tradin.data.data.resource.remote.apis.ProductApi
 import okhttp3.MultipartBody
@@ -17,5 +21,25 @@ class ProductRepositoryImpl @Inject constructor(
 
     override suspend fun postUploadImage(body: MultipartBody.Part): ImageUrlDto {
         return handleResponse(productApi.postUploadImage(body))
+    }
+
+    override suspend fun getProducts(lastId: Int): ProductsDto {
+        return handleResponse(productApi.getProducts(lastId))
+    }
+
+    override suspend fun getProductDetails(id: Int): ProductDto {
+        return handleResponse(productApi.getProductDetails(id))
+    }
+
+    override suspend fun putProduct(body: PutProductBody) {
+        return handleResponse(productApi.putProduct(body))
+    }
+
+    override suspend fun deleteProduct(id: Int) {
+        return handleResponse(productApi.deleteProduct(id))
+    }
+
+    override suspend fun getHistory(): ProductHistoryDto {
+        return handleResponse(productApi.getHistory())
     }
 }

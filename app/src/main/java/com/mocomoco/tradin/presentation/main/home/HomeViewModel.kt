e82578c5f9db498e3160d0c01c28c1a788623279
sorea.no <sorae.no@thingsflow.com>
2022-11-08 00:16:37 +0900

    Feat: Delete home code

--- app/src/main/java/com/mocomoco/tradin/presentation/main/home/HomeViewModel.kt
+++ app/src/main/java/com/mocomoco/tradin/presentation/main/home/HomeViewModel.kt
@@ -1,6 +1,8 @@
 package com.mocomoco.tradin.presentation.main.home
 
 import com.mocomoco.tradin.base.BaseViewModel
+import com.mocomoco.tradin.model.Location
+import com.mocomoco.tradin.model.SortType
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -8,3 +10,16 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor() : BaseViewModel() {
     val a = "asdf"
 }
+
+
+data class HomeState(
+    val location: Location,
+    val sortType: SortType,
+
+
+) {
+//    data class Event(
+//        val
+//    )
+}
+

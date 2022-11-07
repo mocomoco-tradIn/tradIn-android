a23b3279328cff5b2569430ba939f9756ed8ed1e
sorea.no <sorae.no@thingsflow.com>
2022-11-08 00:15:59 +0900

    베이스 뷰모델에 로그 추가

--- app/src/main/java/com/mocomoco/tradin/base/BaseViewModel.kt
+++ app/src/main/java/com/mocomoco/tradin/base/BaseViewModel.kt
@@ -2,6 +2,7 @@ package com.mocomoco.tradin.base
 
 import androidx.lifecycle.ViewModel
 import com.mocomoco.tradin.R
+import com.mocomoco.tradin.common.Logger
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -14,4 +15,12 @@ abstract class BaseViewModel: ViewModel() {
     protected val _toastMessage = MutableSharedFlow<String>()
     val toastMessage = _toastMessage.asSharedFlow()
 
+    init {
+        Logger.logE("init ${this.javaClass.name.split(".").last()}")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Logger.logE("onCleared ${this.javaClass.name.split(".").last()}")
+    }
 }
